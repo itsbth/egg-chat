da4272da1c2c2f9cdcf88eaaaf4907906af2ad21
Bjørn Tore Håvie <bjoern.tore.haavie@pexip.com>
2019-08-12 15:32:17 +0200

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  React.createElement("h1", undefined, "Hello, World"),
+  document.querySelector("#app")
+);
